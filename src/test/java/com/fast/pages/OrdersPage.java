bc4338bbd618efab50b36af9d227723be93db567
ioanaabohus <ioanaa.bohus@gmail.com>
2019-02-14 17:50:04 +0200

    final commit

--- src/test/java/com/fast/pages/OrdersPage.java
+++ src/test/java/com/fast/pages/OrdersPage.java
@@ -4,12 +4,25 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
 @DefaultUrl("http://qa1.fasttrackit.org:8008/my-account/orders/")
 public class OrdersPage extends PageObject {
-    @FindBy (css="td.woocommerce-orders-table__cell-order-number a")
+    @FindBy(css = "td.woocommerce-orders-table__cell.woocommerce-orders-table__cell-order-number")
+    List<WebElementFacade> listOfOrders;
+    @FindBy(css = "td.woocommerce-orders-table__cell-order-number a")
     private WebElementFacade orderNumber;
 
-    public boolean checkExistingOrder (String number) {
-        return orderNumber.containsText(number);
+    public boolean checkExistingOrder(String text) {
+        for (WebElementFacade orderNumber : listOfOrders) {
+            if (orderNumber.getText().contains(text)) {
+                return true;
+            }
+        }
+        return false;
     }
+
 }
+
